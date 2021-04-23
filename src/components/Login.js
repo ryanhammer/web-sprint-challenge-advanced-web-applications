fc52357b2bd750b12653d316187fd2492377c3f8
rhammer35 <rhammer35@icloud.com>
2021-04-23 11:00:50 -0400

    created login form fields and state values to manage form inputs and created login function with axios to send login form credentials to server and get authentication token using axiosWithAuth helper

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,22 +1,98 @@
 import React, { useEffect } from "react";
+import { axiosWithAuth } from '../helpers/axiosWithAuth';
+import { TextField, makeStyles, Button, Grid } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  formItem: {
+    marginBottom: '1.5rem'
+  }
+});
 
 const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
+
+  const history = useHistory();
+  const classes = useStyles();
+
+  const initialLoginValues = {
+    username: "",
+    password: ""
+  };
+
+  const [credentials, setCredentials] = useState(initialLoginValues);
+
+  const handleChange = e => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value
+    });
+  };
+ 
+  const login = e => {
+    e.preventDefault();
+    // Make a POST request with the username and password as the data body to retrieve a token from the api
+    axiosWithAuth()
+      .post('/api/login', credentials)
+      .then( (res) => {
+        // If the post request is store the token in localStorage (sessions, cookies)...add window in front to accommodate some older browsers
+        window.localStorage.setItem('token', JSON.stringify(res.data.payload));
+        // navigate to the BubblePage route
+        history.push('/bubblepage');
+      })
+      .catch( (err) => {
+        console.log(err);
+        setError(err);
+      })
+  };
 
   useEffect(()=>{
     // make a post request to retrieve a token from the api
     // when you have handled the token, navigate to the BubblePage route
   });
   
-  const error = "";
-  //replace with error state
+  const [error, setError] = useState("");
 
   return (
     <div>
       <h1>Welcome to the Bubble App!</h1>
       <div data-testid="loginForm" className="login-form">
-        <h2>Build login form here</h2>
+        <form onSubmit={login} >
+          <Grid 
+            container='true'
+            alignContent="space-evenly"
+            alignItems="center"
+            justify="center"
+            direction="column"
+          >
+            <Grid item className={classes.formItem}>
+              <TextField
+                data-testid="username"
+                name="username"
+                label="Username"
+                type="text"
+                value={credentials.username}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item className={classes.formItem}>
+              <TextField
+                data-testid="password"
+                name="password"
+                label="Password"
+                type="password"
+                value={credentials.password}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Button 
+              className={classes.formItem}
+              variant='contained'
+              color='primary'
+              type='submit'
+              >
+              Login
+            </Button>
+          </Grid>
+        </form>
       </div>
 
       <p data-testid="errorMessage" className="error">{error}</p>
@@ -28,7 +104,7 @@ export default Login;
 
 //Task List:
 //1. Build a form containing a username and password field.
-//2. Add whatever state nessiary for form functioning.
+//2. Add whatever state necessary for form functioning.
 //3. MAKE SURE YOUR USERNAME AND PASSWORD INPUTS INCLUDE data-testid="username" and data-testid="password"
 //4. If either the username or password is not entered, display the following words with the p tag provided: Username or Password not valid.
 //5. If the username / password is equal to Lambda School / i<3Lambd4, save that token to localStorage.
--- src/helpers/axiosWithAuth.js
+++ src/helpers/axiosWithAuth.js
@@ -1,4 +1,14 @@
 import axios from "axios";
 
+export const axiosWithAuth = () => { 
+  // returns a new "instance" of axios with the config object built into it
+  return axios.create({
+    headers: {
+      Authorization: JSON.parse(window.localStorage.getItem('token'))
+    },
+    baseURL: 'http://localhost:5000'
+  });
+};
+
 //Task List:
 //Build and export a function used to send in our authorization token
